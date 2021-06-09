21ee1cb3e234717bf28f07570002cffc840499b5
RFL <63897048+phootrf@users.noreply.github.com>
2021-06-09 21:34:43 +0200

    Update create.sql

--- Milestone 2/create.sql
+++ Milestone 2/create.sql
@@ -1,10 +1,15 @@
 CREATE TABLE trip 
-    (id             SERIAL PRIMARY KEY NOT NULL, --autoinkrementierende Nummer
-     name           VARCHAR(60) NOT NULL,
-     start_date     DATE NOT NULL,  --Format: YYYY-MM-DD
-     end_date       DATE NOT NULL,
-     description    VARCHAR(1024),  --Der String ist hier maximal 1024 Zeichen lang
-     price          DECIMAL(10,2) DEFAULT 0.00,
+    (id                             SERIAL PRIMARY KEY NOT NULL,        --autoinkrementierende Nummer
+     name                           VARCHAR(60) NOT NULL,
+     start_date                     DATE NOT NULL,                      --Format: YYYY-MM-DD
+     end_date                       DATE NOT NULL,
+     description                    VARCHAR(1024),  --Der String ist hier maximal 1024 Zeichen lang
+     price                          DECIMAL(10,2) DEFAULT 0.00,
+     bought_by                      INTEGER NOT NULL,
+     sold_by                        INTEGER NOT NULL,
+     transaction_date               DATE NOT NULL,
+     FOREIGN KEY(bought_by)         REFERENCES client(id),
+     FOREIGN KEY(sold_by)           REFERENCES employee(id),
      CHECK (end_date >= start_date)); --Format: DECIMAL(size, d) mit size = Ziffern, d = Kommastellen.
                                                    --Standardmässig wird für Geldbeträge DECIMAL(19,4) eingesetzt
 CREATE TABLE client
@@ -38,54 +43,37 @@ CREATE TABLE office
 
 CREATE TABLE payment 
     (id             SERIAL PRIMARY KEY NOT NULL,
-     amount         DECIMAL(10,2),
-     payment_date   DATE,
-     payment_method CHAR(11) CHECK (payment_method in ('credit card', 'cash', 'paypal', 'transfer')));
+     amount         DECIMAL(10,2) NOT NULL,
+     date           DATE NOT NULL,
+     method         VARCHAR(15) CHECK (method IN ('credit card', 'cash', 'paypal', 'transfer', 'other')),
+     trip_id        INTEGER NOT NULL,
+     FOREIGN KEY(trip_id) REFERENCES trip(id));
 
 CREATE TABLE activity 
     (id             SERIAL PRIMARY KEY NOT NULL,
      name           VARCHAR(30) NOT NULL,
      description    VARCHAR(1024),
      type           VARCHAR(30),
-     contact        VARCHAR(30)); --Für dieses Feld war mir nicht klar, welcher Datentyp Sinn macht
+     city           VARCHAR(30),
+     country        VARCHAR(30),
+     date           DATE NOT NULL); 
 
 CREATE TABLE transport 
     (id             SERIAL PRIMARY KEY NOT NULL,
-     start          VARCHAR(30),
-     destination    VARCHAR(30),
+     start          VARCHAR(30) NOT NULL,
+     destination    VARCHAR(30) NOT NULL,
      type           VARCHAR(30),
-     transport_date DATE);
+     date           DATE NOT NULL);
 
-CREATE TABLE accomodation 
+CREATE TABLE accommodation 
     (id             SERIAL PRIMARY KEY NOT NULL,
-     city           VARCHAR(30),
-     country        VARCHAR(30),
-     address        VARCHAR(60),
-     begin_date     DATE,
-     overn_stays    INTEGER,
-     type           VARCHAR(30));
-
-CREATE TABLE buys 
-    (client_id      INTEGER NOT NULL REFERENCES client ON DELETE CASCADE,
-     trip_id        INTEGER NOT NULL REFERENCES trip ON DELETE CASCADE,
-     buy_date       DATE,
-     PRIMARY KEY    (trip_id));
-
-CREATE TABLE sells 
-    (employee_id    INTEGER NOT NULL REFERENCES employee ON DELETE CASCADE,
-     trip_id        INTEGER NOT NULL REFERENCES trip ON DELETE CASCADE,
-     sell_date      DATE,
-     PRIMARY KEY    (trip_id));
-
-CREATE TABLE works_at 
-    (employee_id    INTEGER NOT NULL REFERENCES employee ON DELETE CASCADE,
-     office_id      INTEGER NOT NULL REFERENCES office ON DELETE CASCADE,
-     PRIMARY KEY    (employee_id));
-
-CREATE TABLE has_payment 
-    (payment_id     INTEGER NOT NULL REFERENCES payment ON DELETE CASCADE,
-     trip_id        INTEGER NOT NULL REFERENCES trip ON DELETE CASCADE,
-     PRIMARY KEY    (trip_id));
+     city           VARCHAR(30) NOT NULL,
+     country        VARCHAR(30) NOT NULL,
+     address        VARCHAR(60) NOT NULL,
+     name           VARCHAR(60) NOT NULL,
+     begin_date     DATE NOT NULL,
+     duration       INTEGER CHECK (duration > 0),
+     type           VARCHAR(15) CHECK (type IN ('hotel', 'hostel', 'b&b', 'holiday home', 'other'));
 
 CREATE TABLE has_activity 
     (activity_id  INTEGER NOT NULL REFERENCES activity ON DELETE CASCADE,
