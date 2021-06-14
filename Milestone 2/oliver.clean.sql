1ee2e13129a0bf1b2de40274704496824f357363
Christian Pernet <christian.pernet@hotmail.com>
2021-06-14 20:55:38 +0200

    clean update

--- Milestone 2/oliver.clean.sql
+++ Milestone 2/oliver.clean.sql
@@ -1,12 +1,12 @@
-drop table has_activity;
-drop table has_transport;
-drop table has_accommodation;
+drop table if exists has_activity; -- for safety : if the table does not exist, we just receive a notice, not an error;
+drop table if exists has_transport;
+drop table if exists has_accommodation;
 
-drop table activity;
-drop table transport;
-drop table accommodation;
-drop table payment;
-drop table trip;
-drop table client;
-drop table employee;
-drop table office;
+drop table if exists activity;
+drop table if exists transport;
+drop table if exists accommodation;
+drop table if exists payment;
+drop table if exists trip;
+drop table if exists client;
+drop table if exists employee;
+drop table if exists office;
